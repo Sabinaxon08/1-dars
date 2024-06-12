f76b9b6a27903ff3b88dadb592d1e9d7b3536e1f
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-06-12 13:21:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// let son = 3456
+// let son2 = 12489699n
+// let yoqqiymat =null
+// let qiymat = 10>11 
+// let showcase = {
+//     background: " yellow"
+// } 
+
+// console.log(son + " " + typeof son)
+// console.log(son2 + " " + typeof son2)
+// console.log(yoqqiymat + " " + typeof yoqqiymat)
+// console.log(qiymat + " " + typeof qiymat)
+// console.log(showcase + " " + typeof showcase)
+
+
+
+
+let ism = prompt("ism yozing:  ");
+let familiya = prompt(" familiya yozing:  ");
+let yosh = prompt ("yoshingizni yozing:  ");
+let address = prompt ( "address yozing:  ");
+let fan = prompt ("yoqtirgan faningizni yozing:  ");
+
+console.log(ism+" " + typeof ism)
+console.log(familiya + " "+ typeof familiya)
+console.log(yosh + " "+ typeof yosh)
+console.log(address + " "+ typeof address)
+console.log(fan + " "+ typeof fan)
+
+
+
